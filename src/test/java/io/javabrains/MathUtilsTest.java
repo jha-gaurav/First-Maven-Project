7df2dc419447c9347b5b0c18345beb4129ea74f7
jha-gaurav <gaurav.jha02@gmail.com>
2020-05-13 17:17:21 +0530

    Test Pass

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -23,7 +23,7 @@ class MathUtilsTest {
 	@Test
 	void testMul() {
 		MathUtils mus = new MathUtils();
-		int expected = 215;
+		int expected = 15;
 		int actual = mus.multiply(3, 5);
 		assertEquals(expected, actual);
 	}
